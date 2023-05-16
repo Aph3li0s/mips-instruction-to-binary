17b24b121c5a9dc71b544687507d701a69f7d237
Desert Fox <tramphan2294@gmail.com>
2023-05-16 08:13:28 +0700

    add test

---
+++ testcases/test1.asm
@@ -0,0 +1,4 @@
+.text
+add $t1, $zero, 5
+add $t2, $zero, 2
+sub $t3, $t1, $t2
---
+++ testcases/test2.asm
@@ -0,0 +1,18 @@
+.text
+add $t1, $zero, 5 # This is test case 2
+add $t2, $zero, 2 ## Which is more harder
+sub $t3, $t1, $t2
+
+bne $t6, $t3, test
+bne $t1, $t2, adorable
+bne $t5, $t1, cute
+cute: ## Randomly add some random things
+	add $t1, $t1, 1
+
+adorable:
+	add $t1, $t1, 2
+
+test:
+	add $t1, $t1, $t2
+	
+	
