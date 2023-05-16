da024a0e85644114c94817d0318e12439eb8b303
whynotkimhari <108981453+whynotkimhari@users.noreply.github.com>
2023-05-16 12:38:10 +0700

    up dic + idea

--- src/instruction.cpp
+++ src/instruction.cpp
@@ -2,14 +2,45 @@
 //Xác định loại lệnh (R hay I), chuyển các mã lệnh thành binary code
 //Viết thêm 1 hàm chuyển từ binary sang hex để dễ đối chiếu hơn
 
+/*
+    y tuong
+    moi lan xet 1 dong lenh
+    
+    // neu la lenh R
+    if(TYPE[words[0]] == "R") {
+            // Struct: OPCODE + RS + RT + RD + SHAMT + FUNCT
+            if(words[0] != "sll" && words[0] != "srl") {
+                // TODO
+                // These cases: RS = 00000, RD = words[1], RT = words[2], SHAMT = TO_BIN_5(words[3])
+            }
+
+            else {
+                // TODO
+                // Theses cases: RS = words[2], RT = words[3], RD = words[1], SHAMT = 00000
+            }
+        }
+
+        else {  // TYPE I
+            // TODO
+        }
+
+*/
+
 #include "instruction.h"
 #include <iostream>
 #include <vector>
 #include <string>
 #include <cstring>
+#include <map>
 
 using namespace std;
 
+map<string, string> REG;
+map<string, string> OPCODE;
+map<string, string> FUNCT;
+map<string, string> TYPE;
+
+void init();
 vector<string> restructure(char code[255]);
 
 vector<string> restructure(char code[255]) {
@@ -24,4 +55,106 @@ vector<string> restructure(char code[255]) {
         k = strtok(NULL, " ,\t");
     }
     return _;
+}
+
+void init() {
+    // type R
+    TYPE["add"] = "R";
+    TYPE["addu"] = "R";
+    TYPE["and"] = "R";
+    TYPE["jr"] = "R";
+    TYPE["nor"] = "R";
+    TYPE["or"] = "R";
+    TYPE["slt"] = "R";
+    TYPE["sltu"] = "R";
+    TYPE["sll"] = "R";
+    TYPE["srl"] = "R";
+    TYPE["sub"] = "R";
+    TYPE["subu"] = "R";
+
+    // type I
+    TYPE["addi"] = "I";
+    TYPE["addiu"] = "I";
+    TYPE["andi"] = "I";
+    TYPE["beq"] = "I";
+    TYPE["bne"] = "I";
+    TYPE["lb"] = "I";
+    TYPE["lw"] = "I";
+    TYPE["ori"] = "I";
+    TYPE["sb"] = "I";
+    TYPE["sw"] = "I";
+
+    // reg
+    REG["$zero"] = "00000";
+    REG["$at"] = "00001";
+    REG["$v0"] = "00010";
+    REG["$v1"] = "00011";
+    REG["$a0"] = "00100";
+    REG["$a1"] = "00101";
+    REG["$a2"] = "00110";
+    REG["$a3"] = "00111";
+    REG["$t0"] = "01000";
+    REG["$t1"] = "01001";
+    REG["$t2"] = "01010";
+    REG["$t3"] = "01011";
+    REG["$t4"] = "01100";
+    REG["$t5"] = "01101";
+    REG["$t6"] = "01110";
+    REG["$t7"] = "01111";
+    REG["$s0"] = "10000";
+    REG["$s1"] = "10001";
+    REG["$s2"] = "10010";
+    REG["$s3"] = "10011";
+    REG["$s4"] = "10100";
+    REG["$s5"] = "10101";
+    REG["$s6"] = "10110";
+    REG["$s7"] = "10111";
+    REG["$t8"] = "11000";
+    REG["$t9"] = "11001";
+    REG["$k0"] = "11010";
+    REG["$k1"] = "11011";
+    REG["$gp"] = "11100";
+    REG["$sp"] = "11101";
+    REG["$fp"] = "11110";
+    REG["$ra"] = "11111";
+
+    // OPCODE for R ins
+    OPCODE["add"] = "000000";
+    OPCODE["addu"] = "000000";
+    OPCODE["and"] = "000000";
+    OPCODE["jr"] = "000000";
+    OPCODE["nor"] = "000000";
+    OPCODE["or"] = "000000";
+    OPCODE["slt"] = "000000";
+    OPCODE["sltu"] = "000000";
+    OPCODE["sll"] = "000000";
+    OPCODE["srl"] = "000000";
+    OPCODE["sub"] = "000000";
+    OPCODE["subu"] = "000000";
+
+    // OPCODE for I ins
+    OPCODE["addi"] = "001000";
+    OPCODE["addiu"] = "001001";
+    OPCODE["andi"] = "001100";
+    OPCODE["beq"] = "000100";
+    OPCODE["bne"] = "000101";
+    OPCODE["lb"] = "100000";
+    OPCODE["lw"] = "100011";
+    OPCODE["ori"] = "001101";
+    OPCODE["sb"] = "101000";
+    OPCODE["sw"] = "101011";
+
+    // FUNCT for R ins - I ins do not have FUNCT
+    FUNCT["add"] = "100000";
+    FUNCT["addu"] = "100001";
+    FUNCT["and"] = "100100";
+    FUNCT["jr"] = "001000";
+    FUNCT["nor"] = "100111";
+    FUNCT["or"] = "100101";
+    FUNCT["slt"] = "101010";
+    FUNCT["sltu"] = "101011";
+    FUNCT["sll"] = "000000";
+    FUNCT["srl"] = "000010";
+    FUNCT["sub"] = "100010";
+    FUNCT["subu"] = "100011";
 }
