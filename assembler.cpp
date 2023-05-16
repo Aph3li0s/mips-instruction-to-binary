1b03562925ef01073bfc10bbaa5b435bd089885a
whynotkimhari <108981453+whynotkimhari@users.noreply.github.com>
2023-05-16 07:54:19 +0700

    Create assembler.cpp

---
+++ assembler.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<char*> restructure(char code[255], char delim[2] = " ,");
+
+int main() {
+  fstream fi("input.txt");
+    fstream fo("output.txt");
+    string tmp;
+    while(getline(fi, tmp)) {
+        vector<char*> words;
+        char* c_str = new char [tmp.size() + 1];
+        strcpy(c_str, tmp.c_str());
+        words = restructure(c_str);
+      
+        // test ket qua
+        for(auto &word : words) {
+            cout << word << " ";
+        }
+        cout << endl;
+    }
+}
+
+vector<char*> restructure(char code[255], char delim[2] = " ,") {
+    vector<char*> _;
+    auto k = strtok(code, delim);
+    while(k != NULL) {
+        if(*k == '#') break;
+        if(*k != '.') _.push_back(k);
+        k = strtok(NULL, delim);
+    }
+    return _;
+}
