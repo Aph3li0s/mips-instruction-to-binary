d2c421b1950b92b3b7593d7950295214c497913a
Desert Fox <tramphan2294@gmail.com>
2023-05-16 11:47:01 +0700

    add cmt

--- src/instruction.cpp
+++ src/instruction.cpp
@@ -1,3 +1,6 @@
 #include "instruction.h"
 #include <string.h>
 
+//Sử dụng để lưu từ điển
+//Xác định loại lệnh (R hay I), chuyển các mã lệnh thành binary code
+//Viết thêm 1 hàm chuyển từ binary sang hex để dễ đối chiếu hơn
