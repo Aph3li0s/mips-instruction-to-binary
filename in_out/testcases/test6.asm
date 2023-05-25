aa026ff70f8d6f735bed740e9a9cf3792e109ff5
whynotkimhari <108981453+whynotkimhari@users.noreply.github.com>
2023-05-25 21:05:47 +0700

    Add files via upload

---
+++ in_out/testcases/test6.asm
@@ -0,0 +1,19 @@
+.text
+addi                   $t0, ,,,,,,,,,,,,,,,,,,,,,,,,,$zero, 1     #ksdjfkjfksjfksdfs
+addi $t1,                      $zero, 1 #this line asdjdasjdkandandkas
+beq $t1 ,,,,,,,,,,,,,,,,,,, $t0, one #ldfsdfjhksdjfksdjfksdf,sabdfm,hroienrycuiewyrbcnuweynrcuw
+one: #trash aksdkasjdksjdkahdjkbfsodfuvmwaierua
+	add $t0,              $t0, $t1#tashcakscjka shkasa
+	add ,,,,,,,,,,,,,,$t1, ,,,,,,,,,,,,,,,,,,,,,,$t1, $t0
+	beq $t1, ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,$t0, two
+	#ksdjfksjdkfjsdkfjksdfjskdfjksdjfsdmfspomrie9pq8ewvnr9weuripowqeurioqwherjklsdnm
+	two:
+		,,,,,,,,,,,,,,,sub $t2, ,,,,,,,,,,,,,,,,,,,,,,,,,$t1, $t0
+		,,,,,,,,,,,,,addi $t2, ,,,,,,,,,,,,,,,,$t2, ,,,,,,,,,,,,,,,,,10
+		bne ,,,,,,,,,,,,,$t2, $zero, three
+		,,,,,,,,,,,,,,#ksjdfkjsdkfjksdfjksdfjkdsfhk
+		three:
+		,,,,,,,,,,,,,,,,,,,,,,,	beq $zero, $zero, end
+			
+end:#this is endas'dasdjkasdjkasdjlsadkasdjaksdhakdhaskd
+	sll $t2, $t2, 12 #THIS IS ENDASDASNDASNDAS
