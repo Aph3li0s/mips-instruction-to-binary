215ca606c7e0bf31d2131171c5af1ff045266cfe
Desert Fox <tramphan2294@gmail.com>
2023-05-25 21:33:12 +0700

    tests

--- in_out/testcases/test7.asm
+++ in_out/testcases/test7.asm
@@ -2,6 +2,7 @@ addi $s0,,,, $s1,    5 ##$s0 = 5
 addi $s1, $s1, 4
 	sub $s1, $s1, $s0 
 	slt $s2, $s1, $s0
+									#$%$*!(%*^&)???<>":",.{}_+
 fake_label:
 sll $s2, $s2, 2
 beq $s1, $s2, jump
---
+++ in_out/testcases/test9.asm
@@ -0,0 +1,10 @@
+add $t0, $t0, $zero
+#;';./.,.;,;',;;l;123141fkasjg
+Loop: 
+lw $t1, 4($s3)
+
+addi $s3, $s3, 4
+
+
+
+bne $t1, $t0, Loop
